c8626352a175eb1429b8b0d21804bf18f81578de
shashanka2a <37143174+shashanka2a@users.noreply.github.com>
2020-08-30 15:49:37 +0530

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,45 @@
+const TelegramBot = require("node-telegram-bot-api");
+const fs = require("fs");
+const token = "1366924655:AAG40FyYTvMrfb_u6etc2HuEL1SJDc_JdUQ";
+const bot = new TelegramBot(token, { polling: true });
+
+const Nightmare = require("nightmare");
+const nightmare = Nightmare({ show: true });
+
+//const username = "17211a05a5";
+const password = "webcap";
+
+bot.on("message", async (msg) => {
+  const chatId = msg.chat.id;
+  console.log(msg.chat.first_name);
+  const username = msg.text;
+  function f(username, i) {
+    nightmare
+      .viewport(1440, 900)
+      .goto("https://bvrit.edu.in/")
+      .wait(2000)
+      .type("#txtId2", username)
+      .type("#txtPwd2", password)
+      .click("#imgBtn2")
+      .wait(15000)
+      .screenshot("ecap.png")
+      .end()
+      .then(console.log)
+      .catch((error) => {
+        console.error("Search failed:", error);
+      });
+    setTimeout(i, 45000);
+  }
+  function i() {
+    fs.readFile("ecap.png", function (err, data) {
+      if (err) {
+        console.log(err);
+      } else {
+        bot.sendPhoto(chatId, data);
+      }
+    });
+  }
+  f(username, i);
+});
+
+
